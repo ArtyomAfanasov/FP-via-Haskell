f7fbde40107d373fb6e5167834170390c891c8d3
ArtomAfanasov <afanasov.artyom@gmail.com>
2021-01-10 15:56:23 +0300

    Added integrationn via trapezoid method.

--- Part1/Integration.hs
+++ Part1/Integration.hs
@@ -1,2 +1,13 @@
 integration :: (Double -> Double) -> Double -> Double -> Double
-integration f a b = undefined
+integration f a b = 
+    trapezoidMethod 0 a 0
+    where 
+        elementarySegment = 1000
+        h = (b - a) / elementarySegment
+        trapezoidMethod sum x_j step
+            | step == 1000 = sum
+            | otherwise =
+                let 
+                    x_jPlus1 = x_j + h
+                    addendum = (f x_j + f x_jPlus1) / 2 * (x_jPlus1 - x_j)
+                in trapezoidMethod (sum + addendum) (x_jPlus1) (step + 1)
